691895decd306fa1c8e10e02bab263acaff76f1b
Amanpreet Singh <amanpreet321@users.noreply.github.com>
2023-04-26 13:21:56 +0530

    Update Banner.tsx

--- src/app/ui/Banner.tsx
+++ src/app/ui/Banner.tsx
@@ -4,9 +4,9 @@ import BannerPattern from '../../../images/banner_pattern.svg'
 import DrawerPannel from './DrawerPannel';
 export default function Banner() {
   return (
-    <div className='w-full bg-yellow-50' >
-      {/* <Image
-        className='absolute'
+    <div className='w-full relative' >
+      <Image
+        className='absolute z-0'
         src={BannerPattern}
         alt="banner-bg-pattern"
         priority
@@ -17,9 +17,9 @@ export default function Banner() {
         style={{
           objectFit: 'cover',
         }}
-      /> */}
+      />
 
-      <div className='z-10 flex flex-row '>
+      <div className='z-1 flex flex-row relative'>
         <div className='pt-24 pb-24 pl-16 basis-4/6'>
           <h1 className='space-x-2 text-4xl font-bold leading-normal '>I’m Sourav Kumar Pandit<br />
             <span className='text-yellowes '>Full Stack</span> Developer.</h1>
@@ -31,7 +31,7 @@ export default function Banner() {
           <button className='my-6 btn btn-info'>Hire Me</button>
         </div>
 
-        <div className='flex items-center justify-center bg-purple-200 basis-2/6'>
+        <div className='flex items-center justify-center basis-2/6'>
           <div className='rounded-md w-11 h-11 bg-blacky-600'>
 
           </div>
